483cb8273e27047a992fd14f78d13b592fec00b4
peter-ranmarine <peter@ranmarine.io>
2024-10-14 21:41:26 +0200

    okay for real now

--- src/main.rs
+++ src/main.rs
@@ -40,51 +40,57 @@ async fn main() -> Result<(), Error> {
     let midi_handler = Arc::new(Mutex::new(MidiHandler::new()?));
     midi_handler.lock().await.setup_midi_input(shared_state.clone()).await?;
 
+    let mut handles = vec![];
+
     // Sequencers and mixer
     let mut sequencer_a  = EuclideanSequencer::new(
         rx_config_a,
         tx_update_mixer.clone(),
         shared_state.clone());
     sequencer_a.generate_sequence().await;
-    tokio::spawn(async move {
+    handles.push(tokio::spawn(async move {
         sequencer_a.run(0).await.unwrap();
-    });
+    }));
 
     let mut sequencer_b = MarkovSequencer::new(
         rx_config_b,
         tx_update_mixer.clone(),
         shared_state.clone());
     sequencer_b.generate_sequence().await;
-    tokio::spawn(async move {
+    handles.push(tokio::spawn(async move {
         sequencer_b.run(1).await.unwrap();
-    });
+    }));
 
     let mut sequence_mixer = Mixer::new(rx_update_mixer, shared_state.clone());
     sequence_mixer.mix().await;
-    tokio::spawn(async move {
+    handles.push(tokio::spawn(async move {
         sequence_mixer.run().await;
-    });
+    }));
 
     // Input handling
     input::spawn_input_handler(tx_input.clone());
     let shared_state_input = shared_state.clone();
-    tokio::spawn(async move {
+    handles.push(tokio::spawn(async move {
         input::process_input(rx_input, shared_state_input, sequencer_channels).await;
-    });
+    }));
 
     // Playback
     let shared_state_playback = shared_state.clone();
     let midi_handler_clone = midi_handler.clone();
-    tokio::spawn(async move {
+    handles.push(tokio::spawn(async move {
         playback::play(midi_handler_clone, shared_state_playback).await.unwrap();
-    });
+    }));
 
     // Shutdown
-    tokio::spawn(async move {
+    let ctrl_c_handle = tokio::spawn(async move {
         signal::ctrl_c().await.expect("Failed to install Ctrl+C handler");
         println!("Ctrl+C received, exiting...");
         std::process::exit(0);
     });
+
+    tokio::select! {
+        _ = ctrl_c_handle => {},
+    }
 
     Ok(())
 }
